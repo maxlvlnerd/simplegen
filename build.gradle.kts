69208082db57a98a27507b4f0cfc8fcfd16df088
Austin <160679820+maxlvlnerd@users.noreply.github.com>
2024-08-05 01:48:14 -0700

    jitpack stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("maven-publish")
 }
 
 group = "com.github.maxlvlnerd"
@@ -15,7 +16,9 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
-
+java.toolchain {
+    languageVersion.set(JavaLanguageVersion.of(21))
+}
 tasks.test {
     useJUnitPlatform()
 }
